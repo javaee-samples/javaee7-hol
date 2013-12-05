0f14f6a1912d9c39e2c9a70405d276dd7fba4da4
arun-gupta <arun.gupta@gmail.com>
2013-12-04 16:44:34 -0800

    adding PDF conversion steps

--- docs/asciidoc/convert.sh
+++ docs/asciidoc/convert.sh
@@ -1,2 +1,7 @@
+# Convert to HTML
 asciidoctor -a toc -a numbered javaee7-hol.adoc
+
+# Convert to PDF
+asciidoctor -b docbook -a toc -a numbered -d book javaee7-hol.adoc
+~/tools/asciidoctor-fopub-master/fopub javaee7-hol.xml
 
