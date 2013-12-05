25c0d4ae8b44e4c0efbdee578f034556906657d3
arun-gupta <arun.gupta@gmail.com>
2013-12-04 16:56:12 -0800

    some more cleanup after PDF generation

--- docs/asciidoc/convert.sh
+++ docs/asciidoc/convert.sh
@@ -1,7 +1,9 @@
-# Convert to HTML
+echo "Converting to HTML ..."
 asciidoctor -a toc -a numbered javaee7-hol.adoc
+echo "done"
 
-# Convert to PDF
+echo "Converting to PDF ..."
 asciidoctor -b docbook -a toc -a numbered -d book javaee7-hol.adoc
 ~/tools/asciidoctor-fopub-master/fopub javaee7-hol.xml
+echo "done"
 
