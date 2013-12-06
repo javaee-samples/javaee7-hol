9f91b5c3f76b55225c3db096292db045b6863294
arun-gupta <arun.gupta@gmail.com>
2013-12-06 12:05:32 -0800

    Fixing bullets and blending the IntelliJ appendix

--- docs/asciidoc/convert.sh
+++ docs/asciidoc/convert.sh
@@ -2,8 +2,11 @@ echo "Converting to HTML ..."
 asciidoctor -a toc -a numbered javaee7-hol.adoc
 echo "done"
 
-echo "Converting to PDF ..."
+echo "Converting to DocBook ..."
 asciidoctor -b docbook -a toc -a numbered -d book javaee7-hol.adoc
-~/tools/asciidoctor-fopub-master/fopub javaee7-hol.xml
+echo "done"
+
+echo "Converting to PDF ..."
+~/tools/asciidoctor-fopub/fopub javaee7-hol.xml
 echo "done"
 
