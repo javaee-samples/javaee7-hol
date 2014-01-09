f62c110f1bc9056bd16a7530de2556a5eb94b1b6
arun-gupta <arun.gupta@gmail.com>
2014-01-08 21:50:12 -0800

    adding more wildfly specific content

--- docs/asciidoc/convert.sh
+++ docs/asciidoc/convert.sh
@@ -2,11 +2,11 @@ echo "Converting to HTML ..."
 asciidoctor -a toc -a numbered javaee7-hol.adoc
 echo "done"
 
-echo "Converting to DocBook ..."
-asciidoctor -b docbook -a toc -a numbered -d book javaee7-hol.adoc
-echo "done"
+#echo "Converting to DocBook ..."
+#asciidoctor -b docbook -a toc -a numbered -d book javaee7-hol.adoc
+#echo "done"
 
-echo "Converting to PDF ..."
-~/tools/asciidoctor-fopub/fopub javaee7-hol.xml
-echo "done"
+#echo "Converting to PDF ..."
+#~/tools/asciidoctor-fopub/fopub javaee7-hol.xml
+#echo "done"
 
