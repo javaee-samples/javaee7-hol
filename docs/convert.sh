c166e10b770d71b8d18090491125d7a1bae34fa9
arun-gupta <arun.gupta@gmail.com>
2014-01-15 17:41:02 +0100

    Adding Dan's name to acks

--- docs/convert.sh
+++ docs/convert.sh
@@ -8,6 +8,6 @@ asciidoctor -b docbook -a toc -a numbered -d book javaee7-hol.adoc
 echo "done"
 
 echo "Converting to PDF ..."
-/home/dallen/projects/asciidoctor/fopub/fopub javaee7-hol.xml
+~/tools/asciidoctor-fopub/fopub javaee7-hol.xml
 echo "done"
 
