b6429c1a11ee2a175c135022983a7c2b61ca7424
Dan Allen <dan.j.allen@gmail.com>
2014-01-29 02:52:36 -0700

    make use of kbd macro; set shared opts in convert script
    - also correctly label OSX (Mac is not the proper term)

--- docs/convert.sh
+++ docs/convert.sh
@@ -10,14 +10,19 @@
 #
 # ...where the first argument is a comma-delimited list of formats
 
+# Program paths
+ASCIIDOCTOR=asciidoctor
+FOPUB=~/tools/asciidoctor-fopub/fopub
+ASCIIDOCTOR_PDF=~/tools/asciidoctor-pdf/bin/asciidoctor-pdf
+
+# File names
 MASTER_ADOC=javaee7-hol.adoc
 MASTER_DOCBOOK=${MASTER_ADOC/.adoc/.xml}
-ASCIIDOCTOR=asciidoctor
-#FOPUB=~/tools/asciidoctor-fopub/fopub
-#ASCIIDOCTOR_PDF=~/tools/asciidoctor-pdf/bin/asciidoctor-pdf
-FOPUB=~/projects/asciidoctor/fopub/fopub
-ASCIIDOCTOR_PDF=~/projects/asciidoctor/asciidoctor-pdf/bin/asciidoctor-pdf
 
+# Command options
+SHARED_OPTIONS='-a numbered -a experimental -a imagesdir=images'
+
+# Formats
 if [ ! -z $1 ]; then
   read -a FORMATS <<<$(IFS=','; echo $1)
 else
@@ -29,16 +34,16 @@ fi
 for f in ${FORMATS[*]}; do
   if [ $f == 'html' ]; then
     echo "Converting to HTML ..."
-    $ASCIIDOCTOR -v -a numbered -a imagesdir=images $MASTER_ADOC
+    $ASCIIDOCTOR -v $SHARED_OPTIONS $MASTER_ADOC
   elif [ $f == 'docbook' ]; then
     echo "Converting to DocBook ..."
-    $ASCIIDOCTOR -b docbook -a numbered -a imagesdir=images $MASTER_ADOC
+    $ASCIIDOCTOR -b docbook $SHARED_OPTIONS $MASTER_ADOC
   elif [ $f == 'fopdf' ]; then
     echo "Converting to FO-PDF ..."
     $FOPUB $MASTER_DOCBOOK
   elif [ $f == 'pdf' ]; then
     echo "Converting to PDF ..."
-    $ASCIIDOCTOR_PDF -a numbered -a imagesdir=images $MASTER_ADOC
+    $ASCIIDOCTOR_PDF $SHARED_OPTIONS $MASTER_ADOC
   fi
 done
 
